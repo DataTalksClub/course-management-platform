617edb4f6ebd79e48a67851aaf74e4c278fbbca8
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2023-04-24 20:13:31 +0200

    created initial structure, oauth login flow and a dummy index page

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
+
---
+++ leaderboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class LeaderboardConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "leaderboard"
