4bdaf6183e8f1023a6548998d6a39b246643b2a0
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2024-02-10 22:55:58 +0100

    faster scoring

--- courses/scoring.py
+++ courses/scoring.py
@@ -5,17 +5,19 @@ from enum import Enum
 from django.utils import timezone
 from django.db.models import Subquery, OuterRef, Sum
 
+from django.db import transaction
+
+
 from .models import (
     Homework,
     Submission,
     Question,
+    Answer,
     Course,
     QuestionTypes,
     AnswerTypes,
-    Enrollment
+    Enrollment,
 )
-
-
 
 
 logger = logging.getLogger(__name__)
@@ -26,7 +28,9 @@ class HomeworkScoringStatus(Enum):
     FAIL = "Warning"
 
 
-def is_float_equal(value1: str, value2: str, tolerance: float=0.01) -> bool:
+def is_float_equal(
+    value1: str, value2: str, tolerance: float = 0.01
+) -> bool:
     try:
         return abs(float(value1) - float(value2)) <= tolerance
     except ValueError:
@@ -54,13 +58,16 @@ def is_free_form_answer_correct(
     elif answer_type == AnswerTypes.CONTAINS_STRING.value:
         return correct_answer in user_answer
     elif answer_type == AnswerTypes.FLOAT.value:
-        return is_float_equal(user_answer, correct_answer, tolerance=0.01)
+        return is_float_equal(
+            user_answer, correct_answer, tolerance=0.01
+        )
     elif answer_type == AnswerTypes.INTEGER.value:
         return is_integer_equal(user_answer, correct_answer)
 
     return False
 
-def safe_split(value: str, delimiter: str=","):
+
+def safe_split(value: str, delimiter: str = ","):
     if not value:
         return []
 
@@ -90,14 +97,18 @@ def is_answer_correct(question: Question, user_answer: str) -> bool:
     return False
 
 
-def update_score(submission: Submission):
+def update_score(submission: Submission, save: bool = True):
+    logger.info(f"Scoring submission {submission}")
+    updated_answers = []
     questions_score = 0
 
     for answer, question in submission.answers_with_questions:
         is_correct = is_answer_correct(question, answer.answer_text)
 
         answer.is_correct = is_correct
-        answer.save()
+        updated_answers.append(answer)
+        if save:
+            answer.save()
 
         if is_correct:
             questions_score += question.scores_for_correct_answer
@@ -107,60 +118,121 @@ def update_score(submission: Submission):
     learning_in_public_score = 0
 
     if submission.learning_in_public_links:
-        learning_in_public_score = len(submission.learning_in_public_links)
+        learning_in_public_score = len(
+            submission.learning_in_public_links
+        )
         submission.learning_in_public_score = learning_in_public_score
 
-    faq_score = 0    
+    faq_score = 0
 
-    if submission.faq_contribution and len(submission.faq_contribution) >= 5:
+    if (
+        submission.faq_contribution
+        and len(submission.faq_contribution) >= 5
+    ):
         faq_score = 1
         submission.faq_score = faq_score
-    
-    total_score = questions_score + learning_in_public_score + faq_score
+
+    total_score = (
+        questions_score + learning_in_public_score + faq_score
+    )
 
     submission.total_score = total_score
-    submission.save()
+
+    if save:
+        submission.save()
+
+    return submission, updated_answers
+
+
+def score_homework_submissions(
+    homework_id: str,
+) -> tuple[HomeworkScoringStatus, str]:
+    with transaction.atomic():
+        logger.info(f"Scoring submissions for homework {homework_id}")
+
+        homework = Homework.objects.select_for_update().get(
+            pk=homework_id
+        )
+        # homework = Homework.objects.get(pk=homework_id)
+
+        if homework.due_date > timezone.now():
+            return (
+                HomeworkScoringStatus.FAIL,
+                f"The due date for {homework} is in the future. Update the due date to score.",
+            )
+
+        if homework.is_scored:
+            return (
+                HomeworkScoringStatus.FAIL,
+                f"Homework {homework} is already scored.",
+            )
+
+        submissions = Submission.objects.filter(
+            homework__id=homework_id
+        )
+
+        logger.info(
+            f"Scoring {len(submissions)} submissions for {homework}"
+        )
+        updated_submissions = []
+        all_updated_answers = []
+
+        for submission in submissions:
+            updated_submission, updated_answers = update_score(
+                submission, save=False
+            )
+            updated_submissions.append(updated_submission)
+            all_updated_answers.extend(updated_answers)
+
+        logger.info(
+            f"Updating {len(updated_submissions)} submissions for {homework}"
+        )
+
+        Submission.objects.bulk_update(
+            updated_submissions,
+            [
+                "questions_score",
+                "learning_in_public_score",
+                "faq_score",
+                "total_score",
+            ],
+        )
+
+        logger.info(
+            f"Updating {len(all_updated_answers)} answers for {homework}"
+        )
+        Answer.objects.bulk_update(
+            all_updated_answers, ["is_correct"]
+        )
+
+        homework.is_scored = True
+        homework.save()
+
+        update_leaderboard(homework.course)
+
+        logger.info(
+            f"Scored {len(submissions)} submissions for {homework}"
+        )
+
+        return (
+            HomeworkScoringStatus.OK,
+            f"Homework {homework} is scored",
+        )
 
 
 def update_leaderboard(course: Course):
-    scored_homeworks = Homework.objects \
-        .filter(course=course, is_scored=True)
+    scored_homeworks = Homework.objects.filter(
+        course=course, is_scored=True
+    )
 
-    aggregated_scores = Submission.objects \
-        .filter(homework__in=scored_homeworks) \
-        .values('enrollment') \
-        .annotate(total_score=Sum('total_score'))
+    aggregated_scores = (
+        Submission.objects.filter(homework__in=scored_homeworks)
+        .values("enrollment")
+        .annotate(total_score=Sum("total_score"))
+    )
 
     # Update each enrollment with the aggregated score
     for aggregated_score in aggregated_scores:
-        Enrollment.objects \
-            .filter(id=aggregated_score['enrollment']) \
-            .update(total_score=aggregated_score['total_score'])
-
-
-
-def score_homework_submissions(homework_id: str) -> tuple[HomeworkScoringStatus, str]:
-    homework = Homework.objects.get(pk=homework_id)
-
-    if homework.due_date > timezone.now():
-        return (
-            HomeworkScoringStatus.FAIL,
-            f"The due date for {homework} is in the future. Update the due date to score.",
-        )
-
-    if homework.is_scored:
-        return HomeworkScoringStatus.FAIL, f"Homework {homework} is already scored."
-
-    submissions = Submission.objects.filter(homework__id=homework_id)
-
-    for submission in submissions:
-        update_score(submission)
-
-    homework.is_scored = True
-    homework.save()
-
-    update_leaderboard(homework.course)
-
-    logger.info(f"Scored {len(submissions)} submissions for {homework}")
-
-    return HomeworkScoringStatus.OK, f"Homework {homework} is scored"
+        Enrollment.objects.filter(
+            id=aggregated_score["enrollment"]
+        ).update(total_score=aggregated_score["total_score"])
