f9ebad43ea90d1f21ff7278d31ac6a641909ecbf
bhimrazy <bhimrajyadav977@gmail.com>
2024-04-04 11:21:57 +0545

    Add email field to CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class CustomUser(AbstractUser):
@@ -10,5 +9,8 @@ class CustomUser(AbstractUser):
         ('instructor', 'Instructor'),
     )
 
+    email = models.EmailField(_("email address"), max_length=255, unique=True)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='student')
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
