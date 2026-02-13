46237810324e4193156f57ec3d542cf37aa159ab
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-02-13 21:54:30 +0100

    Move admin edit button from leaderboard to score breakdown page

--- courses/tests/test_leaderboard.py
+++ courses/tests/test_leaderboard.py
@@ -5,6 +5,8 @@ from django.utils import timezone
 from datetime import timedelta
 from django.core.cache import cache
 
+from django.urls import reverse
+
 from courses.models import (
     Course,
     Homework,
@@ -155,3 +157,51 @@ class LeaderboardTestCase(TestCase):
 
         # Cache should be invalidated (None)
         self.assertIsNone(cache.get(cache_key))
+
+    def test_score_breakdown_admin_button_visible_for_staff(self):
+        """Test that admin edit button is visible on score breakdown page for staff users"""
+        enrollment = self.create_student("student1")
+        admin_user = User.objects.create_user(username="admin", is_staff=True)
+
+        self.client.force_login(admin_user)
+        url = reverse("leaderboard_score_breakdown", kwargs={
+            "course_slug": self.course.slug,
+            "enrollment_id": enrollment.id,
+        })
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        admin_edit_url = reverse("cadmin_enrollment_edit", kwargs={
+            "course_slug": self.course.slug,
+            "enrollment_id": enrollment.id,
+        })
+        self.assertContains(response, admin_edit_url)
+        self.assertContains(response, "fa-cog")
+
+    def test_score_breakdown_admin_button_hidden_for_regular_user(self):
+        """Test that admin edit button is hidden on score breakdown page for regular users"""
+        enrollment = self.create_student("student1")
+        regular_user = User.objects.create_user(username="regular", is_staff=False)
+
+        self.client.force_login(regular_user)
+        url = reverse("leaderboard_score_breakdown", kwargs={
+            "course_slug": self.course.slug,
+            "enrollment_id": enrollment.id,
+        })
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "fa-cog")
+
+    def test_score_breakdown_admin_button_hidden_for_anonymous(self):
+        """Test that admin edit button is hidden on score breakdown page for anonymous users"""
+        enrollment = self.create_student("student1")
+
+        url = reverse("leaderboard_score_breakdown", kwargs={
+            "course_slug": self.course.slug,
+            "enrollment_id": enrollment.id,
+        })
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "fa-cog")
