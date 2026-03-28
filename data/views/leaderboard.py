491dd1ef5533cd58b4ac9dc60f1e72b9cf428c00
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-03-28 18:56:51 +0100

    Cache leaderboard YAML forever until invalidated

--- data/views/leaderboard.py
+++ data/views/leaderboard.py
@@ -27,7 +27,7 @@ from courses.models.project import ProjectState
 
 logger = logging.getLogger(__name__)
 
-LEADERBOARD_DATA_CACHE_TTL = 3600  # 1 hour
+LEADERBOARD_DATA_CACHE_TTL = None  # Cache forever, invalidated by update_leaderboard()
 
 
 def _build_leaderboard_data(course):
