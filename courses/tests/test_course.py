e2eab6fc085a44fb0cc3031b189bd1edeeec4e9d
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-05-08 21:53:44 +0200

    Show course GitHub link on detail page

--- courses/tests/test_course.py
+++ courses/tests/test_course.py
@@ -190,6 +190,26 @@ class CourseDetailViewTests(TestCase):
             "https://courses.datatalks.club/test-course/register",
         )
 
+    def test_course_detail_shows_github_repo_url(self):
+        self.course.github_repo_url = (
+            "https://github.com/DataTalksClub/test-course"
+        )
+        self.course.save()
+
+        url = reverse(
+            "course", kwargs={"course_slug": self.course.slug}
+        )
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "fab fa-github")
+        self.assertContains(response, "GitHub")
+        self.assertContains(
+            response,
+            "https://github.com/DataTalksClub/test-course",
+        )
+
     def test_course_detail_shows_calendar_feed_link(self):
         url = reverse(
             "course", kwargs={"course_slug": self.course.slug}
