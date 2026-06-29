f1f2f765521b40e708c831b46e48402d76d67035
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-06-29 03:16:58 +0200

    Decompose project submissions view tests

--- courses/tests/test_project_submissions_view.py
+++ courses/tests/test_project_submissions_view.py
@@ -72,16 +72,96 @@ class ProjectSubmissionsViewTests(TestCase):
             project_learning_in_public_score=5,
         )
 
-    def test_submissions_view_unauthenticated_redirects(self):
-        """Test that unauthenticated users are redirected"""
-        url = reverse(
+    def project_submissions_url(self, project=None):
+        return reverse(
             "project_submissions",
+            kwargs={
+                "course_slug": self.course.slug,
+                "project_slug": (project or self.project).slug,
+            },
+        )
+
+    def project_url(self):
+        return reverse(
+            "project",
             kwargs={
                 "course_slug": self.course.slug,
                 "project_slug": self.project.slug,
             },
         )
-        response = self.client.get(url)
+
+    def login_admin(self):
+        self.client.login(username="admin@test.com", password="admin123")
+
+    def create_user_submission(
+        self, index, score=90, commit_id=None
+    ):
+        user = User.objects.create_user(
+            username=f"user{index}@test.com",
+            email=f"user{index}@test.com",
+            password="12345",
+        )
+        enrollment = Enrollment.objects.create(
+            student=user,
+            course=self.course,
+        )
+        return ProjectSubmission.objects.create(
+            project=self.project,
+            student=user,
+            enrollment=enrollment,
+            github_link=f"https://github.com/test/repo{index}",
+            commit_id=commit_id or f"commit{index}",
+            project_score=score,
+        )
+
+    def create_peer_review(self, submission, state):
+        from courses.models import PeerReview, PeerReviewState
+
+        return PeerReview.objects.create(
+            submission_under_evaluation=submission,
+            reviewer=self.submission,
+            state=state,
+            optional=False,
+        )
+
+    def create_peer_review_completion_fixture(self):
+        from courses.models import PeerReviewState
+
+        submission2 = self.create_user_submission(
+            2,
+            score=90,
+            commit_id="def5678",
+        )
+        submission3 = self.create_user_submission(
+            3,
+            score=85,
+            commit_id="ghi9012",
+        )
+        self.create_peer_review(submission2, PeerReviewState.SUBMITTED.value)
+        self.create_peer_review(submission3, PeerReviewState.TO_REVIEW.value)
+
+    def admin_submissions_response(self, project=None):
+        self.login_admin()
+        return self.client.get(
+            self.project_submissions_url(project),
+            follow=True,
+        )
+
+    def submission_for_user(self, response, user):
+        submissions = list(response.context["submissions"])
+        return [
+            submission for submission in submissions
+            if submission.student == user
+        ][0]
+
+    def assert_peer_review_completion(self, response):
+        user_submission = self.submission_for_user(response, self.user)
+        self.assertEqual(user_submission.peer_reviews_completed, 1)
+        self.assertEqual(user_submission.peer_reviews_total, 2)
+
+    def test_submissions_view_unauthenticated_redirects(self):
+        """Test that unauthenticated users are redirected"""
+        response = self.client.get(self.project_submissions_url())
 
         # Should redirect to project view with error message
         self.assertEqual(response.status_code, 302)
@@ -99,14 +179,7 @@ class ProjectSubmissionsViewTests(TestCase):
     def test_submissions_view_regular_user_denied(self):
         """Test that regular users cannot access submissions view"""
         self.client.login(**credentials)
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url)
+        response = self.client.get(self.project_submissions_url())
 
         # Should redirect to project view with error message
         self.assertEqual(response.status_code, 302)
@@ -123,17 +196,7 @@ class ProjectSubmissionsViewTests(TestCase):
 
     def test_submissions_view_admin_can_access(self):
         """Test that admin users can access submissions view"""
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url, follow=True)
+        response = self.admin_submissions_response()
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "cadmin/project_submissions.html")
@@ -148,39 +211,9 @@ class ProjectSubmissionsViewTests(TestCase):
 
     def test_submissions_view_displays_all_submissions(self):
         """Test that all submissions are displayed"""
-        # Create another user and submission
-        user2 = User.objects.create_user(
-            username="user2@test.com",
-            email="user2@test.com",
-            password="12345",
-        )
-        enrollment2 = Enrollment.objects.create(
-            student=user2,
-            course=self.course,
-        )
-        ProjectSubmission.objects.create(
-            project=self.project,
-            student=user2,
-            enrollment=enrollment2,
-            github_link="https://github.com/test/repo2",
-            commit_id="def5678",
-            project_score=90,
-            peer_review_score=45,
-            project_faq_score=8,
-            project_learning_in_public_score=4,
-        )
+        self.create_user_submission(2, score=90, commit_id="def5678")
 
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url, follow=True)
+        response = self.admin_submissions_response()
 
         self.assertEqual(response.status_code, 200)
         submissions = list(response.context["submissions"])
@@ -188,17 +221,8 @@ class ProjectSubmissionsViewTests(TestCase):
 
     def test_admin_link_visible_to_staff(self):
         """Test that the admin link is visible to staff users"""
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url)
+        self.login_admin()
+        response = self.client.get(self.project_url())
 
         self.assertEqual(response.status_code, 200)
         self.assertContains(
@@ -218,14 +242,7 @@ class ProjectSubmissionsViewTests(TestCase):
     def test_admin_link_not_visible_to_regular_users(self):
         """Test that the admin link is not visible to regular users"""
         self.client.login(**credentials)
-        url = reverse(
-            "project",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url)
+        response = self.client.get(self.project_url())
 
         self.assertEqual(response.status_code, 200)
         self.assertNotContains(
@@ -234,96 +251,16 @@ class ProjectSubmissionsViewTests(TestCase):
 
     def test_peer_review_completion_displayed(self):
         """Test that peer review completion is displayed correctly"""
-        from courses.models import PeerReview, PeerReviewState
+        self.create_peer_review_completion_fixture()
 
-        # Create another user and submission
-        user2 = User.objects.create_user(
-            username="user2@test.com",
-            email="user2@test.com",
-            password="12345",
-        )
-        enrollment2 = Enrollment.objects.create(
-            student=user2,
-            course=self.course,
-        )
-        submission2 = ProjectSubmission.objects.create(
-            project=self.project,
-            student=user2,
-            enrollment=enrollment2,
-            github_link="https://github.com/test/repo2",
-            commit_id="def5678",
-            project_score=90,
-        )
-
-        # Create peer reviews for user1
-        # user1 reviews user2's submission (completed)
-        PeerReview.objects.create(
-            submission_under_evaluation=submission2,
-            reviewer=self.submission,
-            state=PeerReviewState.SUBMITTED.value,
-            optional=False,
-        )
-
-        # user1 has another review to do (not completed)
-        user3 = User.objects.create_user(
-            username="user3@test.com",
-            email="user3@test.com",
-            password="12345",
-        )
-        enrollment3 = Enrollment.objects.create(
-            student=user3,
-            course=self.course,
-        )
-        submission3 = ProjectSubmission.objects.create(
-            project=self.project,
-            student=user3,
-            enrollment=enrollment3,
-            github_link="https://github.com/test/repo3",
-            commit_id="ghi9012",
-            project_score=85,
-        )
-        PeerReview.objects.create(
-            submission_under_evaluation=submission3,
-            reviewer=self.submission,
-            state=PeerReviewState.TO_REVIEW.value,
-            optional=False,
-        )
-
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url, follow=True)
+        response = self.admin_submissions_response()
 
         self.assertEqual(response.status_code, 200)
-        
-        # Check that the page contains the peer review completion data
-        submissions = list(response.context["submissions"])
-        user1_submission = [s for s in submissions if s.student == self.user][0]
-        
-        # user1 has completed 1 out of 2 peer reviews
-        self.assertEqual(user1_submission.peer_reviews_completed, 1)
-        self.assertEqual(user1_submission.peer_reviews_total, 2)
+        self.assert_peer_review_completion(response)
 
     def test_copy_emails_button_present(self):
         """Test that the copy emails button is present for admin users"""
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": self.project.slug,
-            },
-        )
-        response = self.client.get(url, follow=True)
+        response = self.admin_submissions_response()
 
         self.assertEqual(response.status_code, 200)
         # Check that the copy button is present
@@ -344,17 +281,7 @@ class ProjectSubmissionsViewTests(TestCase):
             state=ProjectState.COLLECTING_SUBMISSIONS.value,
         )
         
-        self.client.login(
-            username="admin@test.com", password="admin123"
-        )
-        url = reverse(
-            "project_submissions",
-            kwargs={
-                "course_slug": self.course.slug,
-                "project_slug": new_project.slug,
-            },
-        )
-        response = self.client.get(url, follow=True)
+        response = self.admin_submissions_response(new_project)
 
         self.assertEqual(response.status_code, 200)
         # Check that the copy button is not present
