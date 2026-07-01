b56913955a14e0af19e34032801119b0294a34b5
Alexey Grigorev <alexey.s.grigoriev@gmail.com>
2026-07-01 22:25:18 +0200

    Name OpenAPI enum schemas

--- api/openapi/content_schemas/enums.py
+++ api/openapi/content_schemas/enums.py
@@ -4,19 +4,23 @@ from courses.models.project import ProjectState
 
 from ..primitives import choices_schema, enum_schema
 
+HOMEWORK_STATE_SCHEMA = enum_schema(
+    HomeworkState,
+    description="CL=closed, OP=open, SC=scored",
+)
+PROJECT_STATE_SCHEMA = enum_schema(
+    ProjectState,
+    description=(
+        "CL=closed, CS=collecting submissions, PR=peer reviewing, "
+        "CO=completed"
+    ),
+)
+QUESTION_TYPE_SCHEMA = choices_schema(Question.QUESTION_TYPES)
+ANSWER_TYPE_SCHEMA = enum_schema(AnswerTypes, nullable=True)
 
 CONTENT_ENUM_SCHEMAS = {
-    "HomeworkState": enum_schema(
-        HomeworkState,
-        description="CL=closed, OP=open, SC=scored",
-    ),
-    "ProjectState": enum_schema(
-        ProjectState,
-        description=(
-            "CL=closed, CS=collecting submissions, PR=peer reviewing, "
-            "CO=completed"
-        ),
-    ),
-    "QuestionType": choices_schema(Question.QUESTION_TYPES),
-    "AnswerType": enum_schema(AnswerTypes, nullable=True)
+    "HomeworkState": HOMEWORK_STATE_SCHEMA,
+    "ProjectState": PROJECT_STATE_SCHEMA,
+    "QuestionType": QUESTION_TYPE_SCHEMA,
+    "AnswerType": ANSWER_TYPE_SCHEMA,
 }
